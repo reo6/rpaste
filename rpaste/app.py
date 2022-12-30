d1374b060f8b306b22003e7c7c9f161e4b3fe26b
ramazanemreosmanoglu <ramazanemre@protonmail.com>
2022-12-30 21:34:17 +0300

    Add support for hcaptcha.

--- rpaste/app.py
+++ rpaste/app.py
@@ -2,27 +2,36 @@ from flask import Flask, \
                   render_template, \
                   request, \
                   redirect
-from .settings import DB_FILE
+from .settings import DB_FILE, HCAPTCHA
 from tinydb import TinyDB
 from .paste import Paste
+from flask_hcaptcha import hCaptcha
+import os
 
 app = Flask(__name__, static_folder="static")
+app.config["HCAPTCHA_SITE_KEY"] = os.environ["HCAPTCHA_SITE_KEY"]
+app.config["HCAPTCHA_SECRET_KEY"] = os.environ["HCAPTCHA_SECRET_KEY"]
+hcaptcha = hCaptcha(app)
+
 db = TinyDB(DB_FILE)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", ishcaptcha=HCAPTCHA)
 
 @app.route("/save", methods=["POST"])
 def save():
-    print("Saving new..")
-    paste = Paste.create(
-        db,
-        request.form["content"],
-        request.form["description"]
-    )
-    paste.save(db)
-    return redirect(f"/p/{paste.id}")
+    verify = hcaptcha.verify() if HCAPTCHA else True
+    if verify:
+        paste = Paste.create(
+            db,
+            request.form["content"],
+            request.form["description"]
+        )
+        paste.save(db)
+        return redirect(f"/p/{paste.id}")
+    else:
+        return render_template("index.html", ishcaptcha=HCAPTCHA)
 
 @app.route("/p/<id>")
 def get_paste(id):
--- rpaste/settings.py
+++ rpaste/settings.py
@@ -4,3 +4,4 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DB_FILE = BASE_DIR / "db.json"
 ID_LENGTH = 3
 DATE_FORMAT = r"%Y-%m-%d %H:%M"
+HCAPTCHA = True
