11209a93321598a8850b1d94403a3f5866710880
ramazanemreosmanoglu <ramazanemre@protonmail.com>
2022-12-30 17:29:08 +0300

    Implement create paste and get paste functionalities

--- rpaste/app.py
+++ rpaste/app.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template, request
+from flask import Flask, \
+                  render_template, \
+                  request, \
+                  redirect
+from .settings import DB_FILE
+from tinydb import TinyDB
+from .paste import Paste
 
 app = Flask(__name__)
+db = TinyDB(DB_FILE)
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/paste", methods=["POST"])
-def save_paste():
-    pass
+@app.route("/save", methods=["POST"])
+def save():
+    print("Saving new..")
+    paste = Paste.create(
+        db,
+        request.form["content"],
+        request.form["description"]
+    )
+    paste.save(db)
+    return redirect(f"/p/{paste.id}")
+
+@app.route("/p/<id>")
+def get_paste(id):
+    paste = Paste.get(db, id)
+    return render_template("paste.html", paste=paste)
